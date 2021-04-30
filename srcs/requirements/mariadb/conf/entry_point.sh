4969645e93e961ee2c15facf1c561b08db719ab3
aldubar <aldubar@student.42.fr>
2021-04-30 17:30:50 +0200

    maj

--- srcs/requirements/mariadb/conf/entry_point.sh
+++ srcs/requirements/mariadb/conf/entry_point.sh
@@ -9,28 +9,36 @@ OK="[ ${green}OK${clear} ]"
 KO="[ ${red}KO${clear} ]"
 
 
+
+start_daemon()
+{
+cd '/usr' ; /usr/bin/mysqld_safe --datadir='/home/aldubar/data/mariadb'
+}
+
+
 keep_alive()
 {
 killall mariadbd
-mariadbd
+sleep 5
+start_daemon
 }
 
 
 echo -e "${orange}[+] Starting mariadb demon...${clear}"
-mariadbd &
+start_daemon &
 [[ $? -eq 0 ]] && echo -e "${OK}" || echo -e "${KO}"
 
 
 sleep 10
 
 echo -e "${orange}[+] Checking mariadb configuration...${clear}"
-file="/var/lib/mysql/config.txt"
+file="/home/aldubar/data/mariadb/config.txt"
 [ -f "$file" ] && check_config=$( cat "$file" )
 [ "$check_config" = "done" ] && { echo -e "${OK} Config already done. Skipping..."; keep_alive; } || echo -e "Need to create database..."
 
 
 echo -e "${orange}[+] Creating database...${clear}"
-mariadb -e "$(eval "echo \"$(cat create_db.sql)\"")"
+cd /home/mysql; mariadb -e "$(eval "echo \"$(cat create_db.sql)\"")"
 [[ $? -eq 0 ]] && { echo -e "${OK}"; echo "done" > "$file"; keep_alive; } || { echo -e "${KO}"; echo "failed" > "$file"; }
 
 
--- srcs/requirements/wordpress/conf/entry_point.sh
+++ srcs/requirements/wordpress/conf/entry_point.sh
@@ -11,18 +11,21 @@ KO="[ ${red}KO${clear} ]"
 
 keep_alive()
 {
+/etc/init.d/wp_daemon start
 tail -f
 }
 
 
 echo -e "${orange}[+] Checking wordpress config${clear}"
-file="/var/www/localhost/wordpress/config.txt"
+file="/home/aldubar/data/wordpress/config.txt"
 [ -f "$file" ] && check_config=$( cat "$file" )
 [ "$check_config" = "done" ] && { echo -e "${OK} Config already done! Skipping..."; keep_alive; } || echo "Need to configure wordpress..."
 
 
 echo -e "${orange}[+] Configuring wordpress${clear}"
-cp wp-config.php /var/www/localhost/wordpress/wp-config.php
+cp wp-config.php /home/aldubar/data/wordpress/wp-config.php
+cp wp_daemon /etc/init.d/
 [[ $? -eq 0 ]] && { echo -e "${OK} Config done${clear}"; echo "done" > "$file"; keep_alive; } || { echo -e "${KO} Config failed";  echo "failed" > "$file"; }
+
 
 
