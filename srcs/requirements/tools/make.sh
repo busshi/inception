27e4ff5fbfb2e230dba9ffcf8fca61099b5cf571
aldubar <aldubar@student.42.fr>
2021-05-07 09:55:37 +0200

    opti make.sh

--- srcs/requirements/tools/make.sh
+++ srcs/requirements/tools/make.sh
@@ -18,8 +18,7 @@ done
 clean()
 {
 if [[ $(cat /etc/hosts | grep "127.0.0.1 ${DOMAIN_URL}" | wc -l) -gt 0 ]] ; then
-	sudo sed -i "s/127.0.0.1 $DOMAIN_URL//g" /etc/hosts
-	sudo sed -i '/^$/d' /etc/hosts
+	sudo sed -i "/127.0.0.1 $DOMAIN_URL/d" /etc/hosts
 fi
 
 for dir in ${MARIADB_VOLUME_PATH} ${WORDPRESS_VOLUME_PATH} ${FTP_VOLUME_PATH}; do
